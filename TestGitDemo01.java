e6fb74ee205d555f1d44401f9c9dc5a791f7f69a
LzcLzc-Lzc <1612530053@qq.com>
2021-05-16 22:48:09 +0800

    将文件提交到版本库

---
+++ TestGitDemo01.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class TestGitDemo01 {
+    public static void main(String[] args) {
+        System.out.println("TestGitDemo01");
+        System.out.println("TestGitDemo01");
+    }
+    public void add(){
+        System.out.println("这是在student001分支上新增方法");
+    }
+}
