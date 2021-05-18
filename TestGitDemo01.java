639b1fa14b76a99dae597b420d19f5f9cbc2897a
LzcLzc-Lzc <1612530053@qq.com>
2021-05-18 17:50:07 +0800

    提交分支student01

--- TestGitDemo01.java
+++ TestGitDemo01.java
@@ -4,8 +4,7 @@ public class TestGitDemo01 {
     public static void main(String[] args) {
         System.out.println("TestGitDemo01");
         System.out.println("TestGitDemo01");
+        System.out.println("这是student01分支");
     }
-    public void add(){
-        System.out.println("这是在student001分支上新增方法");
-    }
+
 }
